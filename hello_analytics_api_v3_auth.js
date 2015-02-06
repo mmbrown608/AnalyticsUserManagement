e071f01a5dc5b3fceb2b33461d83c430822330dd
mmbrown608 <melissa.brown@veritix.com>
2015-02-06 13:32:25 -0500

    updated auth.js

--- hello_analytics_api_v3_auth.js
+++ hello_analytics_api_v3_auth.js
@@ -19,3 +19,54 @@ function checkAuth() {
   // Pass the response to the handleAuthResult callback function
   gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true}, handleAuthResult);
 }
+
+//Update the UI based on the user's authorization status
+function handleAuthResult(authResult) {
+  if (authResult) {
+    // The user has authorized access
+    // Load the Analytics Client. This function is defined in the next section.
+    loadAnalyticsClient();
+  } else {
+    // User has not Authenticated and Authorized
+    handleUnAuthorized();
+  }
+}
+
+
+// Authorized user
+function handleAuthorized() {
+  var authorizeButton = document.getElementById('authorize-button');
+  var makeApiCallButton = document.getElementById('make-api-call-button');
+
+  // Show the 'Get Sessions' button and hide the 'Authorize' button
+  makeApiCallButton.style.visibility = '';
+  authorizeButton.style.visibility = 'hidden';
+
+  // When the 'Get Sessions' button is clicked, call the makeAapiCall function
+  makeApiCallButton.onclick = makeApiCall;
+}
+
+
+// Unauthorized user
+function handleUnAuthorized() {
+  var authorizeButton = document.getElementById('authorize-button');
+  var makeApiCallButton = document.getElementById('make-api-call-button');
+
+  // Show the 'Authorize Button' and hide the 'Get Sessions' button
+  makeApiCallButton.style.visibility = 'hidden';
+  authorizeButton.style.visibility = '';
+
+  // When the 'Authorize' button is clicked, call the handleAuthClick function
+  authorizeButton.onclick = handleAuthClick;
+}
+
+function handleAuthClick(event) {
+  gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false}, handleAuthResult);
+  return false;
+}
+
+//Create an Analytics Service Object
+function loadAnalyticsClient() {
+  // Load the Analytics client and set handleAuthorized as the callback function
+  gapi.client.load('analytics', 'v3', handleAuthorized);
+}
